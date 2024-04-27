707c873e35c96f4ea31c614dc8cc7340f0126516
grassella <negrete.graciela@yahoo.com>
2024-04-27 12:21:29 -0500

    phase II complete

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,41 @@
-// Your code here 
+const Dragon = require('./dragon');
+
+
+class FriendlyDragon extends Dragon {
+
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color)
+        this.lifeGoals = lifeGoals
+        this.friend = friend
+    }
+
+    hasLifeGoals(args) {
+        
+        for(let el of this.lifeGoals){
+            console.log(`${this.name} likes to ${el}`)
+        }
+      }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+      }
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
+
+
+    module.exports = FriendlyDragon;
+  
